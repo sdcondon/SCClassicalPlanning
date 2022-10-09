c5be77e492472d055071ff9930165e82095fd735
Simon Condon <simoncondon@hotmail.com>
2022-10-09 14:00:38 +0100

    InvariantCheck tweaks

--- src/SCClassicalPlanning/Planning/StateSpaceSearch/Heuristics/InvariantCheck.cs
+++ src/SCClassicalPlanning/Planning/StateSpaceSearch/Heuristics/InvariantCheck.cs
@@ -1,21 +1,20 @@
 ﻿using SCFirstOrderLogic;
 using SCFirstOrderLogic.Inference;
-using System.Xml.Linq;
 
 namespace SCClassicalPlanning.Planning.StateSpaceSearch.Heuristics
 {
     /// <summary>
     /// A decorator heuristic that checks whether the goal violates any known invariants
-    /// before invoking the inner heuristic. If any invariants are violated, returns float.PositiveInfinity.
-    /// Intended to be of use for pruning unreachable goals when backward searching.
+    /// before invoking the inner heuristic. If any invariants are violated, returns <see cref="float.PositiveInfinity"/>.
+    /// Intended to be of use for early pruning of unreachable goals when backward searching.
     /// <para/>
-    /// This heuristic isn't driven by any particular source material,
-    /// but given that it's a fairly obvious idea, I'm assuming it has a name - I'll update it to be
-    /// in keeping with .
+    /// This heuristic isn't driven by any particular source material, but given that it's a fairly
+    /// obvious idea, I'm assuming the approach has a name - I'll update it to use standard terminology as and when.
     /// <para/>
     /// Note that ultimately it should be possible to derive the invariants by examining the problem.
     /// The simplest example of this is if a predicate doesn't appear in any effects. If this is true, the
     /// the occurences of this predicate in the initial state must persist throughout the problem.
+    /// Might research / play with this idea at some point.
     /// </summary>
     public class InvariantCheck
     {
@@ -46,13 +45,17 @@ namespace SCClassicalPlanning.Planning.StateSpaceSearch.Heuristics
                 return 0;
             }
 
-            // Awkwardness - goals are super-easy to convert to CNF, but our knowledge bases want to do that themselves.. 
-            // Meh, never mind.
+            // Performance hit - goals are essentially already in CNF, but our knowledge bases want to do the conversion themselves.. Meh, never mind.
+            // TODO: Perhaps a ToSentence in Goal? (and others..)
             var goalSentence = goal.Elements.Skip(1).Aggregate(goal.Elements.First().ToSentence(), (c, e) => new Conjunction(c, e.ToSentence()));
 
             // Note the negation here. We're not asking if the invariants mean that the goal MUST
-            // be true (that will of course generally not be the case!), we're asking if the state element
-            // CANNOT be true - that is, if its negation must be.
+            // be true (that will of course generally not be the case!), we're asking if the goal
+            // CANNOT be true - that is, if its NEGATION must be true.
+            // TODO: issues with variables in the goal here - which in goals are effectively existentially
+            // quantified, but KBs will i think assume universal. Q.. i *do* wonder if taking the Skolem
+            // function approach for goal vars is a more justifiable approach? But the book makes a
+            // big deal of CP being functionless..
             if (knowledgeBase.Ask(new Negation(goalSentence)))
             {
                 return float.PositiveInfinity;
