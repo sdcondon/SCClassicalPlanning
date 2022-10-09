e465fdce00524d939c209a737c841843d9e7cd05
Simon Condon <simoncondon@hotmail.com>
2022-10-09 10:38:13 +0100

    Added an "InvariantCheck" decorator heuristic - because backward search is a pain..

---
+++ src/SCClassicalPlanning.Tests/Planning/StateSpaceSearch/Heuristics/InvariantCheckTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using FlUnit;
+using SCClassicalPlanning.ExampleDomains.FromAIaMA;
+using SCClassicalPlanning.Planning.StateSpaceSearch.Heuristics;
+using SCFirstOrderLogic;
+using SCFirstOrderLogic.Inference;
+using SCFirstOrderLogic.Inference.Resolution;
+using static SCClassicalPlanning.ExampleDomains.FromAIaMA.AirCargo;
+using static SCClassicalPlanning.ExampleDomains.FromAIaMA.BlocksWorld;
+using static SCClassicalPlanning.ProblemCreation.OperableProblemFactory;
+using static SCFirstOrderLogic.SentenceCreation.OperableSentenceFactory;
+
+namespace SCClassicalPlanning.Planning.StateSpaceSearch
+{
+    public static class InvariantCheckTests
+    {
+        private static readonly Constant blockA = new(nameof(blockA));
+        private static readonly Constant blockB = new(nameof(blockB));
+        private static readonly Constant blockC = new(nameof(blockC));
+
+        private static readonly State BlocksWorldInitialState = new(
+            Block(blockA)
+            & Equal(blockA, blockA)
+            & Block(blockB)
+            & Equal(blockB, blockB)
+            & Block(blockC)
+            & Equal(blockC, blockC)
+            & On(blockA, Table)
+            & On(blockB, Table)
+            & On(blockC, blockA)
+            & Clear(blockB)
+            & Clear(blockC));
+
+        private record TestCase(IEnumerable<Sentence> Invariants, State State, OperableGoal Goal, float ExpectedCost);
+
+        public static Test EstimateCostBehaviour => TestThat
+            .GivenTestContext()
+            .AndEachOf(() => new TestCase[]
+            {
+                new TestCase(
+                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
+                    State: BlocksWorldInitialState,
+                    Goal: Block(Table), // Fine - invariants don't rule this out
+                    ExpectedCost: 0),
+
+                new TestCase(
+                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
+                    State: BlocksWorldInitialState,
+                    Goal: !Block(blockA), // Contradicts Block(blockA)
+                    ExpectedCost: float.PositiveInfinity),
+
+                new TestCase(
+                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
+                    State: BlocksWorldInitialState,
+                    Goal: On(blockA, blockB) & Clear(blockB), // Nope - violates invariant
+                    ExpectedCost: float.PositiveInfinity),
+            })
+            .When((_, tc) =>
+            {
+                var kb = new SimpleResolutionKnowledgeBase(
+                    new SimpleClauseStore(),
+                    SimpleResolutionKnowledgeBase.Filters.None,
+                    SimpleResolutionKnowledgeBase.PriorityComparisons.UnitPreference);
+                kb.Tell(tc.Invariants);
+
+                return new InvariantCheck(kb, (s, g) => 0).EstimateCost(tc.State, tc.Goal);
+            })
+            .ThenReturns()
+            .And((_, tc, rv) => rv.Should().Be(tc.ExpectedCost));
+    }
+}
---
+++ src/SCClassicalPlanning/Planning/StateSpaceSearch/Heuristics/InvariantCheck.cs
@@ -0,0 +1,56 @@
+﻿using SCFirstOrderLogic.Inference;
+
+namespace SCClassicalPlanning.Planning.StateSpaceSearch.Heuristics
+{
+    /// <summary>
+    /// A decorator heuristic that checks whether the goal violates any known invariants
+    /// before invoking the inner heuristic. If any invariants are violated, returns float.PositiveInfinity.
+    /// Intended to be of use for pruning unreachable goals when backward searching.
+    /// <para/>
+    /// Invariants (i.e. facts that are true in any reachable state of a problem) are an important
+    /// concept in classical planning. This heuristic isn't driven by any particular source material,
+    /// but given that it's a fairly obvious idea, I'm assuming it has a name - I'll update it to be
+    /// more .
+    /// <para/>
+    /// Note that ultimately it should be possible to derive the invariants by examining the problem.
+    /// For example, if a predicate doesn't appear in any effects, the occurences in the initial state
+    /// will persist throughout the problem. More complex invariants should be derivable by looking at
+    /// what state changes happen together. It'll be interesting to play with this - but in the meantime
+    /// this heuristic can be used with hand-crafted invariants.
+    /// </summary>
+    public class InvariantCheck
+    {
+        private readonly Func<State, Goal, float> innerHeuristic;
+        private readonly IKnowledgeBase knowledgeBase;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvariantCheck"/>.
+        /// </summary>
+        /// <param name="invariantsKnowledgeBase">A knowledge base containing all of the invariants of the problem.</param>
+        /// <param name="innerHeuristic">the inner heuristic to invoke if no invariants are violated by the goal.</param>
+        public InvariantCheck(IKnowledgeBase invariantsKnowledgeBase, Func<State, Goal, float> innerHeuristic)
+        {
+            this.innerHeuristic = innerHeuristic;
+            this.knowledgeBase = invariantsKnowledgeBase;
+        }
+
+        /// <summary>
+        /// Estimates the cost of getting from the given state to a state that satisfies the given goal.
+        /// </summary>
+        /// <param name="state">The state.</param>
+        /// <param name="goal">The goal.</param>
+        /// <returns>float.PositiveInfinity if any invariants are violated by the goal. Otherwise, the cost estimated by the inner heuristic.</returns>
+        public float EstimateCost(State state, Goal goal)
+        {
+            foreach (var element in goal.Elements)
+            {
+                if (knowledgeBase.Ask(element.Negate().ToSentence()))
+                {
+                    return float.PositiveInfinity;
+                }
+            }
+
+            return innerHeuristic(state, goal);
+        }
+    }
+}
