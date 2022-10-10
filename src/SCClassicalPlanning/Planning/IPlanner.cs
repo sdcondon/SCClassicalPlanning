ad9df6afb43137c034236834e08d15eefb387917
Simon Condon <simoncondon@hotmail.com>
2022-10-10 16:25:14 +0100

    Gah, missed a file from a commit a while back..

--- src/SCClassicalPlanning/Planning/IPlanner.cs
+++ src/SCClassicalPlanning/Planning/IPlanner.cs
@@ -10,6 +10,6 @@
         /// </summary>
         /// <param name="problem">The problem to create a plan for.</param>
         /// <returns>A <see cref="Task"/> representing the process of creating a plan that solves the problem.</returns>
-        Task<Plan> CreatePlanAsync(Problem problem);
+        Task<Plan> CreatePlanAsync(Problem problem, CancellationToken cancellationToken = default);
     }
 }
