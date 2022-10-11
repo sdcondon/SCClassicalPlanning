544da59c599ff2bdb5e9a370b180425c56ca8997
Simon Condon <simoncondon@hotmail.com>
2022-10-11 19:28:13 +0100

    Renamed ElementDifferenceCount to UnsatisfiedGoalCount - simply a much better name, not sure what I was thinking.

--- src/SCClassicalPlanning/Planning/StateSpaceSearch/Heuristics/UnsatisfedGoalCount.cs
+++ src/SCClassicalPlanning/Planning/StateSpaceSearch/Heuristics/UnsatisfedGoalCount.cs
@@ -1,16 +1,16 @@
 ﻿namespace SCClassicalPlanning.Planning.StateSpaceSearch.Heuristics
 {
     /// <summary>
-    /// Very simplistic heuristic that just adds the number of positive elements of the goal that do not occur in the state
-    /// to the number of negative elements that do (that is, it assumes that we need to carry out one action per element of
-    /// the goal that isn't currently satisfied).
+    /// Very simplistic heuristic that just counts the number of unsatisfied goals. That is, it adds the number of positive elements
+    /// of the goal that do not occur in the state to the number of negative elements that do (that is, it assumes that we need to
+    /// carry out one action per element of the goal that isn't currently satisfied).
     /// <para/>
     /// This is generally a pretty terrible heuristic - as are all heuristics that don't take into account the details of the
     /// problem being solved. Consider using something else. DEFINITELY use something else for backward state space searches.
     /// One of the things that this heuristic simply can't do is tell when preconditions are unsatisfiable - which is very bad 
     /// news for a backward state space search, because you rely on being able to do this to prune branches.
     /// </summary>
-    public static class ElementDifferenceCount
+    public static class UnsatisfiedGoalCount
     {
         public static float EstimateCost(State state, Goal goal)
         {
