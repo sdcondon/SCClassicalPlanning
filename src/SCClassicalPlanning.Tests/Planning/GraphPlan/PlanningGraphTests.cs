270c8e463018ed181bb20df54406060bf3d889f0
Simon Condon <simoncondon@hotmail.com>
2022-10-11 15:43:39 +0100

    Planning graph construction

---
+++ src/SCClassicalPlanning.Tests/Planning/GraphPlan/PlanningGraphTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using FlUnit;
+using SCClassicalPlanning.ExampleDomains.FromAIaMA;
+using SCFirstOrderLogic;
+using static SCClassicalPlanning.ExampleDomains.FromAIaMA.BlocksWorld;
+
+namespace SCClassicalPlanning.Planning.GraphPlan
+{
+    public static class PlanningGraphTests
+    {
+        private static readonly Constant blockA = new(nameof(blockA));
+        private static readonly Constant blockB = new(nameof(blockB));
+        private static readonly Constant blockC = new(nameof(blockC));
+        private static readonly Constant Table = new(nameof(Table));
+
+        private record ConstructionTestCase(Problem Problem, ISet<Literal> ExpectedLayer0Propositions);
+
+        public static Test ConstructionBehaviour => TestThat
+            .GivenEachOf(() => new ConstructionTestCase[]
+            {
+                new(
+                    Problem: BlocksWorld.ExampleProblem,
+                    ExpectedLayer0Propositions: new Sentence[]
+                    {
+                        Block(blockA),
+                        Block(blockB),
+                        Block(blockC),
+                        !Block(Table),
+
+                        Equal(blockA, blockA),
+                        !Equal(blockA, blockB),
+                        !Equal(blockA, blockC),
+                        !Equal(blockA, Table),
+                        !Equal(blockB, blockA),
+                        Equal(blockB, blockB),
+                        !Equal(blockB, blockC),
+                        !Equal(blockB, Table),
+                        !Equal(blockC, blockA),
+                        !Equal(blockC, blockB),
+                        Equal(blockC, blockC),
+                        !Equal(blockC, Table),
+                        !Equal(Table, blockA),
+                        !Equal(Table, blockB),
+                        !Equal(Table, blockC),
+                        !Equal(Table, Table), // Fine - we just don't say this - and there's no implicit stuff about equality in the lib
+                        
+                        !On(blockA, blockA),
+                        !On(blockA, blockB),
+                        !On(blockA, blockC),
+                        On(blockA, Table),
+                        !On(blockB, blockA),
+                        !On(blockB, blockB),
+                        !On(blockB, blockC),
+                        On(blockB, Table),
+                        On(blockC, blockA),
+                        !On(blockC, blockB),
+                        !On(blockC, blockC),
+                        !On(blockC, Table),
+                        !On(Table, blockA),
+                        !On(Table, blockB),
+                        !On(Table, blockC),
+                        !On(Table, Table),
+
+                        !Clear(blockA),
+                        Clear(blockB),
+                        Clear(blockC),
+                        !Clear(Table) // Also fine, given how the domain works
+                    }.Select(s => new Literal(s)).ToHashSet()),
+            })
+            .When(tc => new PlanningGraph(tc.Problem))
+            .ThenReturns()
+            .And((tc, rv) => rv.GetPropositions(0).Should().BeEquivalentTo(tc.ExpectedLayer0Propositions.OfType<Literal>()));
+    }
+}
--- src/SCClassicalPlanning/Planning/GraphPlan/PlanningGraph.cs
+++ src/SCClassicalPlanning/Planning/GraphPlan/PlanningGraph.cs
@@ -1,12 +1,128 @@
-﻿namespace SCClassicalPlanning.Planning.GraphPlan
+﻿using SCFirstOrderLogic;
+using SCFirstOrderLogic.SentenceManipulation;
+using SCGraphTheory;
+
+namespace SCClassicalPlanning.Planning.GraphPlan
 {
-    ////public class PlanningGraph
-    ////{
-    ////    private readonly Problem problem;
-    ////
-    ////    public PlanningGraph(Problem problem)
-    ////    {
-    ////        this.problem = problem;
-    ////    }
-    ////}
+    /// <summary>
+    /// Planning graph representation. 
+    /// <para/>
+    /// Lazily populated.
+    /// </summary>
+    public class PlanningGraph
+    {
+        private readonly Problem problem;
+        private readonly List<IEnumerable<Literal>> propositionLayers = new();
+        private readonly List<IEnumerable<Action>> actionLayers = new();
+
+        private int expandedToLevel;
+        //private bool levelledOff;
+        //private int levelsOffAtLayer;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="PlanningGraph"/> class.
+        /// </summary>
+        /// <param name="problem">The problem that the planning graph represents.</param>
+        public PlanningGraph(Problem problem)
+        {
+            this.problem = problem;
+
+            // Planning graphs only work with propositions - no variables allowed.
+            // So here we iterate every possible ground predicate (by subsituting every combination of known constants
+            // for its arguments - add positive if its in the initial state, otherwise negative
+            var layer0 = new List<Literal>();
+            foreach (var predicateTemplate in problem.Domain.Predicates)
+            {
+                foreach (var substitution in ProblemInspector.GetAllPossibleSubstitutions(problem, predicateTemplate, new VariableSubstitution()))
+                {
+                    Predicate predicate = (Predicate)substitution.ApplyTo(predicateTemplate).ToSentence(); // Ugh - why is compiler assuming this overload? because conversion is implicit and method is more concrete?
+
+                    if (problem.InitialState.Elements.Contains(predicate))
+                    {
+                        layer0.Add(predicate);
+                    }
+                    else
+                    {
+                        layer0.Add(new Literal(predicate, true));
+                    }
+                }
+            }
+
+            propositionLayers.Add(layer0);
+            expandedToLevel = 0;
+        }
+
+
+        /// <summary>
+        /// Gets the (ground!) literals that are present at a given layer of the graph - 
+        /// with layer 0 being the initial state.
+        /// </summary>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        public IEnumerable<Literal> GetPropositions(int step)
+        {
+            while (expandedToLevel < step)
+            {
+                throw new NotImplementedException();
+            }
+
+            return propositionLayers[step];
+        }
+
+#if false
+        /// <summary>
+        /// Gets the actions that are available 
+        /// </summary>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        public IEnumerable<Action> GetActions(int step)
+        {
+        }
+
+        /// <summary>
+        /// Interface for nodes of a planning graph.
+        /// </summary>
+        public interface INode : INode<INode, IEdge>
+        {
+
+        }
+
+        /// <summary>
+        /// Interface for nodes of a planning graph.
+        /// </summary>
+        public interface INode : INode<INode, IEdge>
+        {
+
+        }
+
+        public struct PropositionNode : INode
+        {
+
+        }
+
+        /// <summary>
+        /// Container for information about an edge that connects a <see cref="PropositionNode"/> to
+        /// an <see cref="ActionNode"/> in the same layer.
+        /// </summary>
+        public struct ActionEdge : IEdge
+        {
+
+        }
+
+        public struct ActionNode : INode
+        {
+
+        }
+
+        /// <summary>
+        /// Container for information about an edge that connects an <see cref="ActionNode"/> to
+        /// a <see cref="PropositionNode"/> in the next layer.
+        /// </summary>
+        public struct PropositionEdge : IEdge
+        {
+
+        }
+#endif
+
+    }
 }
