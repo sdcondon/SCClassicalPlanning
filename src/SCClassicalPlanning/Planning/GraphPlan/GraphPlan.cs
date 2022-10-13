ed8c0cebcd2fd938286e0dba73353e5e367cda4e
Simon Condon <simoncondon@hotmail.com>
2022-10-13 14:26:47 +0100

    Added GraphPlan placeholder and GraphPlan tests.

---
+++ src/SCClassicalPlanning/Planning/GraphPlan/GraphPlan.cs
@@ -0,0 +1,10 @@
+﻿namespace SCClassicalPlanning.Planning.GraphPlan
+{
+    public class GraphPlan : IPlanner
+    {
+        public Task<Plan> CreatePlanAsync(Problem problem, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
