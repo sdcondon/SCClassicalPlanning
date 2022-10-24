f36cbe889f23bf866a831e1db2a7054828c6dab2
Simon Condon <simoncondon@hotmail.com>
2022-10-24 23:15:40 +0100

    Added non-propositional (i.e. variable preserving - with constraints added to preconditions as necessary) relevant action lookup to DomainInspector.

---
+++ src/SCClassicalPlanning.Tests/Planning/DomainInspectorTests.cs
@@ -0,0 +1,104 @@
+﻿using FluentAssertions;
+using FlUnit;
+using SCClassicalPlanning.ExampleDomains;
+using SCClassicalPlanning.ExampleDomains.FromAIaMA;
+using SCFirstOrderLogic;
+using static SCFirstOrderLogic.SentenceCreation.OperableSentenceFactory;
+using static SCClassicalPlanning.ExampleDomains.Container;
+using static SCClassicalPlanning.ExampleDomains.FromAIaMA.AirCargo;
+
+namespace SCClassicalPlanning.Planning
+{
+    public static class DomainInspectorTests
+    {
+        private static readonly Constant element1 = new(nameof(element1));
+        private static readonly Constant element2 = new(nameof(element2));
+
+        private static readonly Constant sfo = new(nameof(sfo));
+        private static readonly Constant cargo = new(nameof(cargo));
+
+        private record GetRelevantActionsTestCase(Domain Domain, Goal Goal, Action[] ExpectedResult);
+
+        public static Test GetRelevantActionsBehaviour => TestThat
+            .GivenEachOf(() => new GetRelevantActionsTestCase[]
+            {
+                new(
+                    Domain: Container.Domain,
+                    Goal: new(IsPresent(element1)),
+                    ExpectedResult: new[]
+                    {
+                        Add(element1),
+                        new( // Swap element1 in for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(R) & !IsPresent(element1) & !AreEqual(R, element1)),
+                            effect: new(!IsPresent(R) & IsPresent(element1))),
+                    }),
+
+                new(
+                    Domain: Container.Domain,
+                    Goal: new(IsPresent(element1) & IsPresent(element2)),
+                    ExpectedResult: new[]
+                    {
+                        Add(element1),
+                        Add(element2),
+                        new( // Swap element1 in for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(R) & !IsPresent(element1) & !AreEqual(R, element1) & !AreEqual(R, element2)),
+                            effect: new(!IsPresent(R) & IsPresent(element1))),
+                        new( // Swap element2 in for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(R) & !IsPresent(element2) & !AreEqual(R, element2) & !AreEqual(R, element1)),
+                            effect: new(!IsPresent(R) & IsPresent(element2))),
+                    }),
+
+                new(
+                    Domain: Container.Domain,
+                    Goal: new(IsPresent(element1) & !IsPresent(element2)),
+                    ExpectedResult: new[]
+                    {
+                        Add(element1),
+                        Remove(element2),
+                        new( // Swap element1 in for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(R) & !IsPresent(element1) & !AreEqual(R, element1)),
+                            effect: new(!IsPresent(R) & IsPresent(element1))),
+                        new( // Swap element2 out for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(element2) & !IsPresent(A) & !AreEqual(A, element2)),
+                            effect: new(IsPresent(A) & !IsPresent(element2))),
+                    }),
+
+                new(
+                    Domain: Container.Domain,
+                    Goal: new(!IsPresent(element1) & !IsPresent(element2)),
+                    ExpectedResult: new[]
+                    {
+                        Remove(element1),
+                        Remove(element2),
+                        new( // Swap element1 out for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(element1) & !IsPresent(A) & !AreEqual(A, element1) & !AreEqual(A, element2)),
+                            effect: new(IsPresent(A) & !IsPresent(element1))),
+                        new( // Swap element2 out for something else
+                            identifier: "Swap",
+                            precondition: new(IsPresent(element2) & !IsPresent(A) & !AreEqual(A, element2) & !AreEqual(A, element1)),
+                            effect: new(IsPresent(A) & !IsPresent(element2))),
+                    }),
+
+                new(
+                    Domain: AirCargo.Domain,
+                    Goal: new(At(cargo, sfo)),
+                    ExpectedResult: new Action[]
+                    {
+                        Unload(cargo, new VariableReference("plane"), sfo),
+                        new(
+                            identifier: "Fly",
+                            precondition: new(Airport(sfo) & At(cargo, new VariableReference("from")) & !AreEqual(new VariableReference("from"), sfo) & Plane(cargo) & Airport(new VariableReference("from"))),
+                            effect: new(!At(cargo, new VariableReference("from")) & At(cargo, sfo))),
+                    }),
+            })
+            .When(tc => DomainInspector.GetRelevantActions(tc.Domain, tc.Goal))
+            .ThenReturns()
+            .And((tc, r) => r.Should().BeEquivalentTo(tc.ExpectedResult));
+    }
+}
--- src/SCClassicalPlanning/Planning/DomainInspector.cs
+++ src/SCClassicalPlanning/Planning/DomainInspector.cs
@@ -1,4 +1,5 @@
-﻿using SCFirstOrderLogic;
+﻿using SCClassicalPlanning.ProblemManipulation;
+using SCFirstOrderLogic;
 using SCFirstOrderLogic.SentenceManipulation;
 using SCFirstOrderLogic.SentenceManipulation.Unification;
 
@@ -72,6 +73,133 @@ namespace SCClassicalPlanning.Planning
 
             // TODO: I suspect its possible for this to return more than one result.. investigate, write tests
             return MatchWithSchema(action, schema, new VariableSubstitution()).Single();
+        }
+
+        /// <summary>
+        /// Gets the (action schema, variable substitution) pairings that represent actions that are relevant to a given goal in a given problem.
+        /// </summary>
+        /// <param name="domain">The domain of the problem being solved.</param>
+        /// <param name="goal">The goal to retrieve the relevant actions for.</param>
+        /// <returns>The actions that are relevant to the given state.</returns>
+        public static IEnumerable<(Action schema, VariableSubstitution substitution, Goal constraints)> GetRelevantActionSchemaSubstitutions(Domain domain, Goal goal)
+        {
+            // Local method to check that a given unifier results in an unconstrained non-match with the negation of the elements of the goal.
+            // Worth it because ExpandNonMatchesWithGoalNegation is potentially so expensive.
+            bool TryGetConstraints(IEnumerable<Literal> effectElements, VariableSubstitution substitution, out Goal constraints)
+            {
+                List<Literal> constraintElements = new();
+
+                foreach (var effectElement in effectElements)
+                {
+                    foreach (var goalElement in goal.Elements)
+                    {
+                        var clashingSubstitution = new VariableSubstitution(substitution);
+
+                        if (LiteralUnifier.TryUpdateUnsafe(goalElement, substitution.ApplyTo(effectElement).Negate(), clashingSubstitution))
+                        {
+                            var constrainedVars = clashingSubstitution.Bindings.Where(k => !substitution.Bindings.ContainsKey(k.Key));
+
+                            if (!constrainedVars.Any())
+                            {
+                                // We didn't need to specify any further vars to get a conflict - so this substitution won't work at all.
+                                constraints = null;
+                                return false;
+                            }
+                            else
+                            {
+                                foreach (var kvp in constrainedVars)
+                                {
+                                    constraintElements.Add(new Literal(new Predicate(EqualitySymbol.Instance, kvp.Key, kvp.Value), true));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                constraints = new Goal(constraintElements);
+                return true;
+            }
+
+            // Local method to create variable subsitutions such that the effects elements transformed by the substitution contain at least one match to the goal's elements.
+            // effectElements: The elements of the effect to be matched.
+            // returns: An enumerable of VariableSubstitutions that can be applied to the effect elements to make at least one of the match a goal element
+            IEnumerable<VariableSubstitution> MatchWithGoal(IEnumerable<Literal> effectElements)
+            {
+                foreach (var effectElement in effectElements)
+                {
+                    // Here we iterate through all elements of the goal, trying to find unifications with the effect element.
+                    // We return each unification we find immediately - for an effect to be relevant it only needs to match at least one element of the goal.
+                    foreach (var goalElement in goal.Elements)
+                    {
+                        if (LiteralUnifier.TryCreate(goalElement, effectElement, out var unifier))
+                        {
+                            yield return unifier;
+                        }
+                    }
+                }
+            }
+
+            foreach (var schema in domain.Actions)
+            {
+                // TODO: "standardise" here.
+
+                foreach (var substitution in MatchWithGoal(schema.Effect.Elements).Distinct())
+                {
+                    if (TryGetConstraints(schema.Effect.Elements, substitution, out var constraints))
+                    {
+                        yield return (schema, substitution, constraints);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the actions that are relevant to a given goal in a given problem.
+        /// </summary>
+        /// <param name="domain">The domain of the problem being solved.</param>
+        /// <param name="goal">The goal to retrieve the relevant actions for.</param>
+        /// <returns>The actions that are relevant to the given state.</returns>
+        public static IEnumerable<Action> GetRelevantActions(Domain domain, Goal goal)
+        {
+            foreach (var (Schema, Substitution, Constraints) in GetRelevantActionSchemaSubstitutions(domain, goal))
+            {
+                yield return new VariableSubstitutionActionTransformation(Substitution, Constraints).ApplyTo(Schema);
+            }
+        }
+
+        /// <summary>
+        /// Utility class to transform <see cref="Action"/> instances using a given <see cref="VariableSubstitution"/>.
+        /// </summary>
+        private class VariableSubstitutionActionTransformation : RecursiveActionTransformation
+        {
+            private readonly VariableSubstitution substitution;
+            private readonly Goal constraints;
+
+            public VariableSubstitutionActionTransformation(VariableSubstitution substitution, Goal constraints)
+            {
+                this.substitution = substitution;
+                this.constraints = constraints;
+            }
+
+            public override Action ApplyTo(Action action)
+            {
+                action = base.ApplyTo(action);
+
+                if (!constraints.Equals(Goal.Empty))
+                {
+                    // There are constraints - add them to the precondition of the action
+                    return new Action(
+                        identifier: action.Identifier,
+                        precondition: new Goal(action.Precondition.Elements.Concat(constraints.Elements)),
+                        effect: action.Effect);
+                }
+                else
+                {
+                    return action;
+                }
+            }
+
+            public override Literal ApplyTo(Literal literal) => substitution.ApplyTo(literal);
         }
     }
 }
