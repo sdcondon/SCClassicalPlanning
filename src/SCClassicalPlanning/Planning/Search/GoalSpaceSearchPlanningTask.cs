dc7943744324d50439fc23132b6407f6da6ef98b
Simon Condon <simoncondon@hotmail.com>
2023-01-18 14:20:01 +0000

    Refactoring (breaking change): IPlanningTask implementations are no longer public inner types. There was no reason for this.

---
+++ src/SCClassicalPlanning/Planning/Search/GoalSpaceSearchPlanningTask.cs
@@ -0,0 +1,180 @@
+﻿// Copyright 2022-2023 Simon Condon
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using SCClassicalPlanning.Planning.Utilities;
+using SCGraphTheory;
+using SCGraphTheory.Search.Classic;
+using System.Collections;
+
+namespace SCClassicalPlanning.Planning.Search
+{
+    /// <summary>
+    /// The implementation of <see cref="IPlanningTask"/> used by <see cref="GoalSpaceSearch"/>.
+    /// </summary>
+    public class GoalSpaceSearchPlanningTask : SteppablePlanningTask<(Goal, Action, Goal)>
+    {
+        private readonly AStarSearch<GoalSpaceNode, GoalSpaceEdge> search;
+
+        private bool isComplete;
+        private Plan? result;
+
+        internal GoalSpaceSearchPlanningTask(Problem problem, IStrategy strategy)
+        {
+            search = new AStarSearch<GoalSpaceNode, GoalSpaceEdge>(
+                source: new GoalSpaceNode(problem, problem.Goal),
+                isTarget: n => problem.InitialState.Satisfies(n.Goal),
+                getEdgeCost: e => strategy.GetCost(e.Action),
+                getEstimatedCostToTarget: n => strategy.EstimateCost(problem.InitialState, n.Goal));
+
+            CheckForSearchCompletion();
+        }
+
+        /// <inheritdoc />
+        public override bool IsComplete => isComplete;
+
+        /// <inheritdoc />
+        public override bool IsSucceeded => result != null;
+
+        /// <inheritdoc />
+        public override Plan Result
+        {
+            get
+            {
+                if (!IsComplete)
+                {
+                    throw new InvalidOperationException("Task is not yet complete");
+                }
+                else if (result == null)
+                {
+                    throw new InvalidOperationException("Plan creation failed");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public override (Goal, Action, Goal) NextStep()
+        {
+            if (IsComplete)
+            {
+                throw new InvalidOperationException("Task is complete");
+            }
+
+            var edge = search.NextStep();
+            CheckForSearchCompletion();
+
+            return (edge.From.Goal, edge.Action, edge.To.Goal);
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            // Nothing to do
+            GC.SuppressFinalize(this);
+        }
+
+        private void CheckForSearchCompletion()
+        {
+            if (search.IsConcluded)
+            {
+                if (search.IsSucceeded)
+                {
+                    result = new Plan(search.PathToTarget().Reverse().Select(e => e.Action).ToList());
+                }
+
+                isComplete = true;
+            }
+        }
+
+        private readonly struct GoalSpaceNode : INode<GoalSpaceNode, GoalSpaceEdge>, IEquatable<GoalSpaceNode>
+        {
+            private readonly Problem problem;
+
+            public GoalSpaceNode(Problem problem, Goal goal) => (this.problem, Goal) = (problem, goal);
+
+            /// <summary>
+            /// Gets the goal represented by this node.
+            /// </summary>
+            public Goal Goal { get; }
+
+            /// <inheritdoc />
+            public IReadOnlyCollection<GoalSpaceEdge> Edges => new GoalSpaceNodeEdges(problem, Goal);
+
+            /// <inheritdoc />
+            public override bool Equals(object? obj) => obj is GoalSpaceNode node && Equals(node);
+
+            /// <inheritdoc />
+            // NB: this struct is private - so we don't need to look at the problem, since it'll always match
+            public bool Equals(GoalSpaceNode node) => Equals(Goal, node.Goal);
+
+            /// <inheritdoc />
+            public override int GetHashCode() => HashCode.Combine(Goal);
+
+            /// <inheritdoc />
+            public override string ToString() => Goal.ToString();
+        }
+
+        private readonly struct GoalSpaceNodeEdges : IReadOnlyCollection<GoalSpaceEdge>
+        {
+            private readonly Problem problem;
+            private readonly Goal goal;
+
+            public GoalSpaceNodeEdges(Problem problem, Goal goal) => (this.problem, this.goal) = (problem, goal);
+
+            /// <inheritdoc />
+            public int Count => ProblemInspector.GetRelevantActions(problem, goal).Count();
+
+            /// <inheritdoc />
+            public IEnumerator<GoalSpaceEdge> GetEnumerator()
+            {
+                foreach (var action in ProblemInspector.GetRelevantActions(problem, goal))
+                {
+                    yield return new GoalSpaceEdge(problem, goal, action);
+                }
+            }
+
+            /// <inheritdoc />
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        private readonly struct GoalSpaceEdge : IEdge<GoalSpaceNode, GoalSpaceEdge>
+        {
+            private readonly Problem problem;
+            private readonly Goal fromGoal;
+
+            public GoalSpaceEdge(Problem problem, Goal fromGoal, Action action)
+            {
+                this.problem = problem;
+                this.fromGoal = fromGoal;
+                this.Action = action;
+            }
+
+            /// <inheritdoc />
+            public GoalSpaceNode From => new(problem, fromGoal);
+
+            /// <inheritdoc />
+            public GoalSpaceNode To => new(problem, Action.Regress(fromGoal));
+
+            /// <summary>
+            /// Gets the action that is regressed over to achieve this goal transition.
+            /// </summary>
+            public Action Action { get; }
+
+            /// <inheritdoc />
+            public override string ToString() => new PlanFormatter(problem.Domain).Format(Action);
+        }
+    }
+}
---
+++ src/SCClassicalPlanning/Planning/Search/StateSpaceSearchPlanningTask.cs
@@ -0,0 +1,161 @@
+﻿// Copyright 2022-2023 Simon Condon
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using SCClassicalPlanning.Planning.Utilities;
+using SCGraphTheory;
+using SCGraphTheory.Search.Classic;
+using System.Collections;
+
+namespace SCClassicalPlanning.Planning.Search
+{
+    /// <summary>
+    /// The implementation of <see cref="IPlanningTask"/> used by <see cref="StateSpaceSearch"/>.
+    /// </summary>
+    public class StateSpaceSearchPlanningTask : SteppablePlanningTask<(State, Action, State)>
+    {
+        private readonly AStarSearch<StateSpaceNode, StateSpaceEdge> search;
+
+        private bool isComplete;
+        private Plan? result;
+
+        internal StateSpaceSearchPlanningTask(Problem problem, IStrategy strategy)
+        {
+            search = new AStarSearch<StateSpaceNode, StateSpaceEdge>(
+                source: new StateSpaceNode(problem, problem.InitialState),
+                isTarget: n => n.State.Satisfies(problem.Goal),
+                getEdgeCost: e => strategy.GetCost(e.Action),
+                getEstimatedCostToTarget: n => strategy.EstimateCost(n.State, problem.Goal));
+
+            CheckForSearchCompletion();
+        }
+
+        /// <inheritdoc />
+        public override bool IsComplete => isComplete;
+
+        /// <inheritdoc />
+        public override bool IsSucceeded => result != null;
+
+        /// <inheritdoc />
+        public override Plan Result
+        {
+            get
+            {
+                if (!IsComplete)
+                {
+                    throw new InvalidOperationException("Task is not yet complete");
+                }
+                else if (result == null)
+                {
+                    throw new InvalidOperationException("Plan creation failed");
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public override (State, Action, State) NextStep()
+        {
+            if (IsComplete)
+            {
+                throw new InvalidOperationException("Task is complete");
+            }
+
+            var edge = search.NextStep();
+            CheckForSearchCompletion();
+
+            return (edge.From.State, edge.Action, edge.To.State);
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            // Nothing to do
+            GC.SuppressFinalize(this);
+        }
+
+        private void CheckForSearchCompletion()
+        {
+            if (search.IsConcluded)
+            {
+                if (search.IsSucceeded)
+                {
+                    result = new Plan(search.PathToTarget().Select(e => e.Action).ToList());
+                }
+
+                isComplete = true;
+            }
+        }
+
+        private readonly struct StateSpaceNode : INode<StateSpaceNode, StateSpaceEdge>, IEquatable<StateSpaceNode>
+        {
+            private readonly Problem problem;
+
+            public StateSpaceNode(Problem problem, State state) => (this.problem, State) = (problem, state);
+
+            public State State { get; }
+
+            public IReadOnlyCollection<StateSpaceEdge> Edges => new StateSpaceNodeEdges(problem, State);
+
+            public override bool Equals(object? obj) => obj is StateSpaceNode node && Equals(node);
+
+            // NB: this struct is private - so we don't need to look at the problem, since it'll always match
+            public bool Equals(StateSpaceNode node) => Equals(State, node.State);
+
+            public override int GetHashCode() => HashCode.Combine(State);
+        }
+
+        private readonly struct StateSpaceNodeEdges : IReadOnlyCollection<StateSpaceEdge>
+        {
+            private readonly Problem problem;
+            private readonly State state;
+
+            public StateSpaceNodeEdges(Problem problem, State state) => (this.problem, this.state) = (problem, state);
+
+            public int Count => ProblemInspector.GetApplicableActions(problem, state).Count();
+
+            public IEnumerator<StateSpaceEdge> GetEnumerator()
+            {
+                foreach (var action in ProblemInspector.GetApplicableActions(problem, state))
+                {
+                    yield return new StateSpaceEdge(problem, state, action);
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        private readonly struct StateSpaceEdge : IEdge<StateSpaceNode, StateSpaceEdge>
+        {
+            private readonly Problem problem;
+            private readonly State fromState;
+
+            public StateSpaceEdge(Problem problem, State state, Action action)
+            {
+                this.problem = problem;
+                this.fromState = state;
+                this.Action = action;
+            }
+
+            public StateSpaceNode From => new(problem, fromState);
+
+            public StateSpaceNode To => new(problem, Action.ApplyTo(fromState));
+
+            public Action Action { get; }
+
+            public override string ToString() => new PlanFormatter(problem.Domain).Format(Action);
+        }
+    }
+}
