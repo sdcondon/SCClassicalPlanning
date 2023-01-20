859041d3429b8d052f3580fc27d7151634355a37
Simon Condon <simoncondon@hotmail.com>
2023-01-20 12:27:37 +0000

    using directive tidy-up

--- src/SCClassicalPlanning/Planning/Search/StateSpaceSearch.cs
+++ src/SCClassicalPlanning/Planning/Search/StateSpaceSearch.cs
@@ -11,10 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-using SCClassicalPlanning.Planning.Utilities;
-using SCGraphTheory;
-using SCGraphTheory.Search.Classic;
-using System.Collections;
 
 namespace SCClassicalPlanning.Planning.Search
 {
