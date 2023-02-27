1174b03d8f8c420bb1668550d79ca5c6ec87859e
Simon Condon <simoncondon@hotmail.com>
2023-02-27 21:31:31 +0000

    Use a mock KB in InvariantInspectorTests.

--- src/SCClassicalPlanning.Tests/Planning/Utilities/InvariantInspectorTests.cs
+++ src/SCClassicalPlanning.Tests/Planning/Utilities/InvariantInspectorTests.cs
@@ -1,11 +1,9 @@
 ﻿using FluentAssertions;
 using FlUnit;
+using SCClassicalPlanning._TestUtilities;
 using SCFirstOrderLogic;
-using SCFirstOrderLogic.Inference;
-using SCFirstOrderLogic.Inference.Resolution;
 using static SCClassicalPlanning.ExampleDomains.FromAIaMA.BlocksWorld;
 using static SCClassicalPlanning.ProblemCreation.OperableProblemFactory;
-using static SCFirstOrderLogic.SentenceCreation.OperableSentenceFactory;
 
 namespace SCClassicalPlanning.Planning.Utilities
 {
@@ -14,44 +12,39 @@ namespace SCClassicalPlanning.Planning.Utilities
         private static readonly Constant blockA = new(nameof(blockA));
         private static readonly Constant blockB = new(nameof(blockB));
 
-        private record TestCase(IEnumerable<Sentence> Invariants, OperableGoal Goal, bool ExpectedResult);
+        private record TestCase(Dictionary<Sentence, bool> KnowledgeBaseResponses, OperableGoal Goal, bool ExpectedResult);
 
         public static Test IsGoalPrecludedByInvariantsBehaviour => TestThat
             .GivenEachOf(() => new TestCase[]
             {
-                new TestCase( // Empty goal - always fine
-                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
+                new TestCase( // Empty goal
+                    KnowledgeBaseResponses: new(),
                     Goal: Goal.Empty,
                     ExpectedResult: false),
 
-                new TestCase( // Element always true - fine
-                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
-                    Goal: Block(Table),
+                new TestCase( // Trivial goal, invariants not violated
+                    KnowledgeBaseResponses: new() { [!Block(blockA)] = false },
+                    Goal: Block(blockA),
                     ExpectedResult: false),
 
-                new TestCase( // Violates Block(blockA)
-                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
-                    Goal: !Block(blockA),
+                new TestCase( // Trivial goal, invariant violated
+                    KnowledgeBaseResponses: new() { [!Block(Table)] = true },
+                    Goal: Block(Table),
                     ExpectedResult: true),
 
-                new TestCase( // Violates on/clear relationship
-                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
-                    Goal: On(blockA, blockB) & Clear(blockB),
-                    ExpectedResult: true),
-
-                new TestCase( // on/clear relationship is relevant but not violated - fine
-                    Invariants: new Sentence[] { Block(blockA), ForAll(A, B, If(On(A, B), !Clear(B))) },
+                new TestCase( // Non-trivial goal - invariants not violated
+                    KnowledgeBaseResponses: new() { [!(On(blockB, blockA) & Clear(blockB))] = false },
                     Goal: On(blockB, blockA) & Clear(blockB),
                     ExpectedResult: false),
+
+                new TestCase( // Non-trivial goal - invariant violated
+                    KnowledgeBaseResponses: new() { [!(On(blockA, blockB) & Clear(blockB))] = true },
+                    Goal: On(blockA, blockB) & Clear(blockB),
+                    ExpectedResult: true),
             })
             .When(tc =>
             {
-                var kb = new ResolutionKnowledgeBase(new DelegateResolutionStrategy(
-                    new HashSetClauseStore(),
-                    DelegateResolutionStrategy.Filters.None,
-                    DelegateResolutionStrategy.PriorityComparisons.None)); // No point in unitpreference, 'cos query is *all* unit clauses..
-                kb.Tell(tc.Invariants);
-
+                var kb = new MockKnowledgeBase(tc.KnowledgeBaseResponses);
                 return new InvariantInspector(kb).IsGoalPrecludedByInvariants(tc.Goal);
             })
             .ThenReturns()
