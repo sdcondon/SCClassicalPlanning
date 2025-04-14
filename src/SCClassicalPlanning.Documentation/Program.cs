26774d6f774ab2a80bd437cfcd96d368cc739e30
Simon Condon <simon@sdcondon.net>
2025-04-14 13:32:41 +0100

    docs site: added blazorise & respect system color mode preference

--- src/SCClassicalPlanning.Documentation/Program.cs
+++ src/SCClassicalPlanning.Documentation/Program.cs
@@ -1,11 +1,24 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.Bootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SCClassicalPlanning.Documentation;
+using SCClassicalPlanning.Documentation.Razor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddBootstrapIcons()
+    .AddScoped(sp => new HttpClient
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    });
 
 await builder.Build().RunAsync();
